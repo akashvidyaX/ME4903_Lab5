41dd2fb2d4623f65d9439fe54f0659f5e34a1e7b
mfdunlap <76010127+mfdunlap@users.noreply.github.com>
2023-09-22 11:29:53 -0500

    Update grubler_formula_1.py
    - Change f1, f2, and f3 to ask how many joints have 1/2/3 DoF
    - Update Grubler's formula to reflect f1, f2, and f3 changes
    - Update docstring for degrees_of_freedom()

--- grubler_formula_1.py
+++ grubler_formula_1.py
@@ -6,15 +6,15 @@ def degrees_of_freedom(m, N, J, f1, f2, f3):
     m: The number of degrees of freedom of a single body.
     N: The number of links in the system.
     J: The number of joints in the system.
-    f1: The number of degrees of freedom of joint 1.
-    f2: The number of degrees of freedom of joint 2.
-    f3: The number of degrees of freedom of joint 3.
+    f1: The number of joints with 1 degree of freedom.
+    f2: The number of joints with 2 degree of freedom.
+    f3: The number of joints with 3 degree of freedom.
 
   Returns:
-    The number of degrees of freedom of the mechanism.
+    dof: The number of degrees of freedom of the mechanism.
   """
 
-  return m * (N - 1 - J) + f1 + f2 + f3
+  return m * (N - 1 - J) + f1 + (2 * f2) + (3 * f3)
 
 def main():
   """
@@ -24,9 +24,9 @@ def main():
   m = int(input("Enter the number of degrees of freedom of a single link: "))
   N = int(input("Enter the number of links in the system: "))
   J = int(input("Enter the number of joints in the system: "))
-  f1 = int(input("Enter the number of degrees of freedom of joint 1: "))
-  f2 = int(input("Enter the number of degrees of freedom of joint 2: "))
-  f3 = int(input("Enter the number of degrees of freedom of joint 3: "))
+  f1 = int(input("Enter the number of joints that have 1 degree of freedom: "))
+  f2 = int(input("Enter the number of joints that have 2 degree of freedom: "))
+  f3 = int(input("Enter the number of joints that have 3 degrees of freedom: "))
 
   dof = degrees_of_freedom(m, N, J, f1, f2, f3)
   print("The number of degrees of freedom is", dof)
